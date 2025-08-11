6b63719f41439d4f8b66dca07b776f69d0901544
Quang-Minh TRAN <Minhounet@users.noreply.github.com>
2025-08-11 21:47:42 +0200

    Add problem 2

---
+++ src/main/java/com/aerow/training/day1/Problem2.java
@@ -0,0 +1,35 @@
+package com.aerow.training.day1;
+/*
+ * Calculer le nombre à partir d'un chiffre romain.
+ * I = 1
+ * II = 2
+ * III = 3
+ * IV = 4
+ * V = 5
+ * ...
+ * IX = 9
+ *
+ * X = 10
+ *
+ * XL = 40
+ * L = 50
+ * LX = 60
+ *
+ * XC = 90
+ * C = 100
+ *
+ * CD = 400
+ * D = 500
+ * DC = 600
+ *
+ * M = 1000
+ * MM = 2000
+ * MMM = 3000
+ */
+public class Problem2 {
+
+    public static int toDecimal(String romanNumber) {
+        return 0;
+    }
+    
+}
