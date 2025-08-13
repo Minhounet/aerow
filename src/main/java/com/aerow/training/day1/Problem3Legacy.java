cae9b0e89cea6b394eae2f4b7b79b620344fb90a
Quang-Minh TRAN <Minhounet@users.noreply.github.com>
2025-08-13 22:38:54 +0200

    ☣️Refactor last time

--- src/main/java/com/aerow/training/day1/Problem3Legacy.java
+++ src/main/java/com/aerow/training/day1/Problem3Legacy.java
@@ -1,5 +1,7 @@
 package com.aerow.training.day1;
 
+import java.util.Map;
+
 /*
  * Code legacy, sécuriser avec des tests d'abord.
  *
@@ -9,23 +11,27 @@ package com.aerow.training.day1;
  */
 public class Problem3Legacy {
 
+    private static final Map<String, String> FRENCH_LEVELS = Map.of(
+            "D", "Facile",
+            "B", "Difficile",
+            "A", "Très Difficile"
+    );
+
+    private static final Map<String, String> ENGLISH_LEVELS = Map.of(
+            "D", "Easy",
+            "B", "Hard",
+            "A", "Very Hard"
+    );
+    public static final String LEVEL_EXPERT = "Expert";
+
+
     public static String translateLevel(String levelCode, String language) {
         if ("C".equals(levelCode)) {
             return "Normal";
         }
         if (language.equals("fr")) {
-            return switch (levelCode) {
-                case "B" -> "Difficile";
-                case "A" -> "Très Difficile";
-                case "D" -> "Facile";
-                default -> "Expert";
-            };
+            return FRENCH_LEVELS.getOrDefault(levelCode, LEVEL_EXPERT);
         }
-        return switch (levelCode) {
-            case "D" -> "Easy";
-            case "B" -> "Hard";
-            case "A" -> "Very Hard";
-            default -> "Expert";
-        };
+        return ENGLISH_LEVELS.getOrDefault(levelCode, LEVEL_EXPERT);
     }
 }
