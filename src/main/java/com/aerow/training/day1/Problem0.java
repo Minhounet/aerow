94017fe901f28323cd578ecdadb84299f6d97f0a
Quang-Minh TRAN <Minhounet@users.noreply.github.com>
2025-08-14 15:43:06 +0200

    Add problem 0

---
+++ src/main/java/com/aerow/training/day1/Problem0.java
@@ -0,0 +1,14 @@
+package com.aerow.training.day1;
+
+/*
+ * Compter le nombre de caractères total des mots se terminant par e ou commençant pas a
+ *
+ * "Adonne toi à la joie d'avoir un alibi : "Adonne" "joie" "alibi" = 15
+ *
+ */
+public class Problem0 {
+
+    public static int count(String sentence) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
