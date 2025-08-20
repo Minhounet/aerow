30a168210a9022332ebeeb441b1fc3ca2b9a948c
Quang-Minh TRAN <Minhounet@users.noreply.github.com>
2025-08-20 15:51:33 +0200

    Summit pb 4

---
+++ src/main/java/com/aerow/training/day1/Problem4.java
@@ -0,0 +1,22 @@
+package com.aerow.training.day1;
+
+import java.util.List;
+
+/*
+ * Entrée : liste de String
+ * Sortie : liste d'entiers de la forme suivante:
+ * - il s'agit de la taille de chaque mot multiplé par deux
+ * - mettre des 0 entre chaque nombre
+ *
+ * Exemple : ["je", "suis", "ici", "et", "là"]
+ * Sortie : [2, 0, 4, 0, 2, 0, 2]
+ *
+ * consigne : pas de boucle ! que du fonctionnel
+ *
+ */
+public class Problem4 {
+
+    public List<Integer> funkyTransform(List<String> input) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
