a94818bdb2686ab5f97dbeab778d2c48a46c2548
Quang-Minh TRAN <Minhounet@users.noreply.github.com>
2025-08-21 14:35:58 +0200

    Submit pb 7

---
+++ src/main/java/com/aerow/training/day1/Problem7.java
@@ -0,0 +1,10 @@
+package com.aerow.training.day1;
+
+/*
+ * Soit une liste de nombre, faire la somme des carrés de manière récursive et tail rec.
+ *
+ * vous avez le droit de récupérer le premier élément de la liste et de faire un sublist pour récupérer le reste
+ * on évite d'utiliser du tout fait pour l'exercice
+ */
+public class Problem7 {
+}
