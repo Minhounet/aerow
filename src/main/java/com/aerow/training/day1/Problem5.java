c59a9476829dba634314a678a2fbaf15c1ce3e91
Quang-Minh TRAN <Minhounet@users.noreply.github.com>
2025-08-21 10:24:54 +0200

    Submit pb 5

---
+++ src/main/java/com/aerow/training/day1/Problem5.java
@@ -0,0 +1,16 @@
+package com.aerow.training.day1;
+
+import java.util.List;
+
+/*
+ *  On te donne une liste de chaînes de caractères qui représente des entiers
+ * (mais certains ne sont pas valides, ex: "42", "abc", "100", "0", "-5").
+ *
+ * Renvoyer la somme au carré des chaînes de caractères représentant des entier (positif ou négatif)
+ */
+public class Problem5 {
+
+    public static int funkySum(List<String> tokens) {
+       throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
