efa6c6c5d87b20a8f737dac93a489198f5f5ed24
Quang-Minh TRAN <Minhounet@users.noreply.github.com>
2025-08-21 10:37:05 +0200

    Submit pb 6

---
+++ src/main/java/com/aerow/training/day1/Problem6.java
@@ -0,0 +1,13 @@
+package com.aerow.training.day1;
+
+/*
+ * Soit une phrase :
+ * Renvoyer un entier qui correspond à la division (troncature) entre le nombre de lettres et le nombre de mots.
+ * Exemple: "j'aime cette formation" => 18/4 = 4
+ */
+public class Problem6 {
+
+    public static int specialDivide(String sentence) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
