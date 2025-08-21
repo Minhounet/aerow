67497797b965f2f9dbf3fc9bc32ecafc2c8af9c0
Quang-Minh TRAN <Minhounet@users.noreply.github.com>
2025-08-21 10:38:36 +0200

    Add note for pb 6

--- src/main/java/com/aerow/training/day1/Problem6.java
+++ src/main/java/com/aerow/training/day1/Problem6.java
@@ -3,7 +3,9 @@ package com.aerow.training.day1;
 /*
  * Soit une phrase :
  * Renvoyer un entier qui correspond à la division (troncature) entre le nombre de lettres et le nombre de mots.
- * Exemple: "j'aime cette formation" => 18/4 = 4
+ * Exemple: "j'aime cette formation" => 18/4 = 4.
+ *
+ * Comme d'habitude, pas de boucle etc...
  */
 public class Problem6 {
 
