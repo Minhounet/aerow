60b5119962fa390dd8d6f38a987381b4c10962f2
Quang-Minh TRAN <Minhounet@users.noreply.github.com>
2025-08-27 23:45:21 +0200

    Fix junit launcher

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "3.5.5"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "fr.aerow.training"
@@ -11,16 +13,13 @@ repositories {
 
 dependencies {
     implementation("io.vavr:vavr:0.10.5")
+    implementation("org.springframework.boot:spring-boot-starter")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("org.mockito:mockito-core:5.19.0")
     testImplementation("org.mockito:mockito-junit-jupiter:5.10.0")
 }
-
-tasks.test {
-    useJUnitPlatform()
-}
-
 
 java {
     toolchain {
