feb7356c8e15dada4c2fe88d75088e672eb70d59
Quang-Minh TRAN <Minhounet@users.noreply.github.com>
2025-08-27 21:02:23 +0200

    Fix script again baby

--- internal/move_solution.sh
+++ internal/move_solution.sh
@@ -5,7 +5,7 @@ echo "INFO: Move solution and its branch on top of master"
 git switch master
 git pull --rebase
 git rebase master solution --update-refs
-git branch | grep -v master | grep -v solution | xargs -I {} git push origin {}
-git push origin solution
+git branch | grep -v master | grep -v solution | xargs -I {} git push origin {} --force-with-lease
+git push origin solution --force-with-lease
 git switch master
 echo "👍"
