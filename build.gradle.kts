c8c58455acc28a8cfa1760f4e232343570da9b6a
Quang-Minh TRAN <Minhounet@users.noreply.github.com>
2025-08-29 16:11:31 +0200

    Init h2 part

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,9 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.5.5")
+    implementation("com.h2database:h2:2.3.232")
+
     // La super lib programmation fonctionnelle
     implementation("io.vavr:vavr:0.10.5")
     // spring
